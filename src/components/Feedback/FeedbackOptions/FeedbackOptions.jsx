55c9b71db930647a2f2cbf5056a9f458ee16950b
ArtemPrihnich <artem.prihnich@gmail.com>
2022-09-16 17:06:39 +0300

    задание 1 доработка

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -2,14 +2,13 @@ import PropTypes from 'prop-types'
 import { Item, List, Button } from './FeedbcakOptions.styled'
 
 export default function FeedbackOptions({ options, onLeaveFeedback }) {
+
+    const elements = options.map(option => (
+        <Item key={option}><Button onClick={() => onLeaveFeedback(`${option}`)}>{option}</Button></Item>
+    ))
+
     return (
-        <>
-            <List>
-                {options.map(option => (
-                    <Item key={option}><Button onClick={() => onLeaveFeedback(`${option}`)}>{option}</Button></Item>
-                ))}
-            </List>
-        </>
+        <List>{elements}</List>
     )
 }
 
